5d87a51c1c6cd97c81f4549555ea344a2efa5d96
Hodofe Ernest <hodofeernest@gmail.com>
2026-07-21 16:53:38 +0000

    ...

--- numbers/functions.py
+++ numbers/functions.py
@@ -1,10 +1,17 @@
+# today's tutorial is on functions in python
+
+# defining of a function
+
+def sum(a, b):
+    sum = a + b
+    print(sum)
+
+
+# calling a function
+sum(10, 20)
 
 
 
-def greet(name):
-    print("hello " + name + " welcome to the program")
-
-    greet("john")
 
 
 
