a2b1d0e1f843f050293702739410efe14d40fce6
Hodofe Ernest <hodofeernest@gmail.com>
2026-07-31 09:39:36 +0000

    ...

---
+++ start/arithmetic/math operations/if statements/statement1.py
@@ -0,0 +1,22 @@
+
+Price_of_house = 1000000
+
+has_good_credit = True
+
+if has_good_credit:
+    print("You need to put down 10% ")
+
+    down_payment1 = 0.1 * Price_of_house
+
+    print(f"Your down payment is: ${down_payment1}" )
+
+
+else:
+    print("You need to put down 20%")
+
+    down_payment2 = 0.2 * Price_of_house
+    print(f"Your down payment is: ${down_payment2}" )
+
+
+
+
