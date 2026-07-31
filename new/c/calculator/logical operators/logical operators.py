dbc18763c4e2f7a79d8ad6d3d307c7f7181764ef
Hodofe Ernest <hodofeernest@gmail.com>
2026-07-31 09:49:55 +0000

    ...

---
+++ new/c/calculator/logical operators/logical operators.py
@@ -0,0 +1,16 @@
+has_high_income = False
+has_good_credit = True
+
+   #for AND  operator both conditions must be true
+if has_high_income and has_good_credit:
+
+    print("Eligible for loan")
+
+else:
+    print("Not eligible for the loan")
+
+#for OR operator at least one condition must be true
+
+
+ 
+    
