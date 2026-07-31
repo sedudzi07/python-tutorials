d66255e7023bf80a1fff49902a4c9c701c214342
Hodofe Ernest <hodofeernest@gmail.com>
2026-07-31 10:54:29 +0000

    ...

---
+++ new/c/calculator/logical operators/comparison operators.py
@@ -0,0 +1,10 @@
+temperature = 31
+
+if temperature > 30:
+    print("It is a hot day")
+
+
+else:
+    print("It is not a hot day")
+
+print("Enjoy your day")
---
+++ new/c/calculator/logical operators/comparison3.py
@@ -0,0 +1,18 @@
+
+name =input("What is your name?")
+
+if(len(name)) < 5:
+    print("Characters must be at least 5")
+    print("Try again")
+
+elif len(name) == 50:
+
+    print("name is too long")
+
+else:
+    print("Name looks good")
+
+
+
+
+
