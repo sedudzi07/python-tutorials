06f65b4efa93410570de60841bf849cd347ca58c
Hodofe Ernest <hodofeernest@gmail.com>
2026-07-31 09:16:52 +0000

    ...

---
+++ start/arithmetic/math operations/if statements/if statements.py
@@ -0,0 +1,17 @@
+is_hot = False
+is_cold = False
+
+if is_hot:
+
+    print("it is a hot day")
+    print("drink plenty of water")
+
+elif is_cold:
+
+    print("it is a cold day")
+    print("wear warm clothes")
+
+else:
+    print("it is a loveley day")
+
+    print("enjoy your day")
