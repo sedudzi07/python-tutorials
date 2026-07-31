7d4b00b21a163644cb63aab6b76131d21b1634d8
Hodofe Ernest <hodofeernest@gmail.com>
2026-07-31 08:15:01 +0000

    ...

---
+++ start/arithmetic/arithmetic.py
@@ -0,0 +1,56 @@
+#addition
+print(30 + 5) 
+
+#subtraction
+print(30 -5)
+
+#division
+print(30 / 5)
+
+#multiplication
+print(30 * 5)
+
+#modulus or remainder
+
+print(30 % 5)
+
+#index or power
+print(30 ** 5)
+
+x = 10
+x = x + 3
+print(x)
+
+#augmented assignment operator
+
+y = 30
+y += 3
+print(y)
+
+a = 20
+a -=3
+print(a)
+
+b = 11
+b *=4
+print(b)
+
+#operator precedence
+s = 10 + 2 * 3
+#what do you think will be the results
+print(s)
+# order of precedence:
+# parenthesis
+# exponentiation
+# multiplication or division
+# addtion or subtraction
+
+e = 2 ** 5 + 4 * 5 - 4
+print (e)
+
+p = (20 + 4) * 4 - 3
+print(p)
+
+
+
+
