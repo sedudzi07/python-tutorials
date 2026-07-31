8eded9967462fb1341cdae2feae88cc91a71813e
Hodofe Ernest <hodofeernest@gmail.com>
2026-07-31 08:47:16 +0000

    ...

---
+++ start/arithmetic/math operations/math operations.py
@@ -0,0 +1,20 @@
+import math
+
+x = 2.44
+y = 4.51
+#rounding of numbers
+print(round(x))
+print(round(y))
+
+#finding the absolute value of numbers
+d = -44.3
+print(abs(d))
+
+#ceiling of a number
+print(math.ceil(x))
+
+#floor of a number
+print(math.floor(y))
+
+v = 3
+print(math.factorial(v))
