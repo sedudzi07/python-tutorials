00b1bc5b290132cce57060c25de2f135d01d714d
Hodofe Ernest <hodofeernest@gmail.com>
2026-08-01 23:21:09 +0000

    ...

---
+++ new/nl.py
@@ -0,0 +1,24 @@
+
+
+weight = int(input("enter your weight"))
+
+unit = input("is this in L(bs) or K(g)? ")
+
+if unit.upper() == "L":
+    converted = weight *0.45
+
+    print(f("your weight is {converted} in kg"))
+
+
+else: 
+    unit.upper()== "K"
+
+    converted = weight/0.45
+
+    print(f"your weight is {converted} in pounds)")
+
+
+
+
+
+ 
