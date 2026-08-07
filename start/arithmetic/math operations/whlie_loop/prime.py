2621dc61a1daca60eda18de1aeefee21ebd39c02
Hodofe Ernest <hodofeernest@gmail.com>
2026-08-07 22:36:55 +0000

    ...

--- start/arithmetic/math operations/whlie_loop/prime.py
+++ start/arithmetic/math operations/whlie_loop/prime.py
@@ -1,20 +1,14 @@
-account_balance = 1000
+password = "Swagga"
 
-amount_to_withdraw = float(input("Enter the amount you wanna withdraw: "))
+while password != password
 
-
-
-if amount_to_withdraw > account_balance:
-    print("Insufficint funds")
-    print("Your account balance is: " ,(account_balance))
-    print("you need to withdraw exact amount or less than the amount")
-   
+print("access denied")
+    
 
 
 
 
-elif amount_to_withdraw <= account_balance:
-    print("Successful transaction")
-    print("Your new account balance is: ", account_balance - amount_to_withdraw)
 
-    print("Thank you")
+
+
+
