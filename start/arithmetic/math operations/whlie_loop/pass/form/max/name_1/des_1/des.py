b548b980ad9b9aa700900aeb267882be9cb6e30e
Hodofe Ernest <hodofeernest@gmail.com>
2026-09-21 00:06:28 +0000

    ...

--- start/arithmetic/math operations/whlie_loop/pass/form/max/name_1/des_1/des.py
+++ start/arithmetic/math operations/whlie_loop/pass/form/max/name_1/des_1/des.py
@@ -1,28 +1,9 @@
+x = [2, 4, 33, 54, 6, 87, 123, 5467, 79,]
 
-for x in range(1, 10):
-    print(x)
-
-print("we will now reverse the process")
-
-for y in reversed(range(1, 21)):
-    print(y)
-
-print("now let us do skip a number in the range")
-
-for number in range(1, 6):
-    if number == 4:
-        continue
-    else:
-        print(number)
-
-print(" as you can see, 4 have been skipped")
-
-print("now let us break out of the loop when we reacj a certain number")
-
-tele = ("0531552185")
-for tele in tele:
-    if number == 2:
-        break
-    else:
-        print(tele)
+max = x[0]
+for x in x:
+    if  x > max:
+        max = x
     
+
+print(max)
---
+++ start/arithmetic/math operations/whlie_loop/pass/form/max/name_1/des_1/list/list.py
@@ -0,0 +1,20 @@
+numbers = [2, 3, 6, 5, 7, 5, 9, 13,]
+
+#numbers.insert(0, 30)
+
+#numbers.reverse()
+
+#numbers.remove(5)
+
+#numbers.clear()
+
+#numbers.pop()
+
+print(12 in numbers)
+
+numbers.sort()
+numbers.reverse()
+print(numbers)
+#print(numbers.index(6))
+#print(numbers.count(5))
+
